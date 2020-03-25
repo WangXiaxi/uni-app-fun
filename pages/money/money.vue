08891b334ed3e0d09fbfdf228d5b2b88eede861f
WangXiaoXi <995107408@qq.com>
2020-03-25 22:28:43 +0800

    add

---
+++ pages/money/money.vue
@@ -0,0 +1,242 @@
+<template>
+	<view class="content">
+		<view class="top-part">
+			<image class="bg" src="../../static/qbbj.png"></image>
+			<view class="box">
+				<image src="../../static/gywm2.png"></image>
+				<view class="info">
+					<view>{{userInfo.mobile}}</view>
+					<view>账户余额：{{detail.Money | nf}}元</view>
+					<view>到期时间：{{detail.Time | fill}}</view>
+				</view>
+			</view>
+
+		</view>
+		<view class="form-box">
+			<view class="title">话费充值卡</view>
+			<view class="row b-b">
+				<text class="tit">卡号</text>
+				<input class="input" type="text" v-model="formData.code" placeholder="请输入充值卡号" placeholder-class="placeholder" />
+			</view>
+			<view class="row b-b">
+				<text class="tit">卡密</text>
+				<input class="input" type="password" v-model="formData.pass" placeholder="请输入充值卡密" placeholder-class="placeholder" />
+			</view>
+		</view>
+		<view class="tips">1、卡号和卡密是唯一的，请仔细输入</view>
+		<button class="add-btn" @click="confirm" :loading="btnLoading" :disabled="btnLoading">立即充值</button>
+		<mix-loading v-if="pageLoading"></mix-loading>
+	</view>
+</template>
+
+<script>
+	import {
+		mapGetters,
+		mapActions,
+		mapMutations
+	} from 'vuex'
+	import phoneModel from '../../api/phone.js'
+	import mixLoading from '../../components/mix-loading.vue'
+	const formData = {
+		code: '',
+		pass: ''
+	}
+	export default {
+		components: {
+			mixLoading
+		},
+		data() {
+			return {
+				detail: {},
+				pageLoading: false,
+				formData: JSON.parse(JSON.stringify(formData)),
+				btnLoading: false,
+				rules: {
+					cardno: {
+						required: true
+					},
+					password: {
+						required: true
+					}
+				},
+				messages: {
+					cardno: {
+						required: '请输入卡号！'
+					},
+					password: {
+						required: '请输入密码！'
+					}
+				}
+			}
+		},
+		computed: {
+			...mapGetters(['tokenPhone', 'userInfo'])
+		},
+		onLoad() {
+			this.loadData()
+		},
+		methods: {
+			loadData() {
+				this.pageLoading = true
+				phoneModel.getCallBalance({
+					token: this.tokenPhone
+				}).then(res => {
+					this.detail = res.data.json
+					this.pageLoading = false
+				}).catch(() => {
+					this.pageLoading = false
+				})
+			},
+			confirm() {
+				const {
+					formData: {
+						code,
+						pass
+					},
+					mobile,
+					token,
+					rules,
+					messages
+				} = this
+				const sendData = {
+					mobile: this.userInfo.mobile,
+					cardno: code,
+					password: pass,
+					token: this.tokenPhone
+				}
+				phoneModel.initValidate(rules, messages)
+				if (!phoneModel.WxValidate.checkForm(sendData)) return
+				this.btnLoading = true
+				phoneModel.rechangeCall(sendData).then(res => {
+					this.btnLoading = false
+					this.formData = JSON.parse(JSON.stringify(formData))
+					this.$api.msg('恭喜您，充值成功！')
+					this.loadData()
+				}).catch(() => {
+					this.btnLoading = false
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+	page {
+		background: #FFFFFF;
+	}
+
+	.tips {
+		color: #909399;
+		font-size: 26upx;
+		line-height: 60upx;
+		padding-left: 32upx;
+	}
+
+	.top-part {
+		position: relative;
+		width: 100%;
+		height: 370upx;
+		text-align: left;
+		padding-left: 32upx;
+		padding-top: 80upx;
+
+		.box {
+			position: relative;
+			z-index: 1;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			image {
+				width: 120upx;
+				height: 120upx;
+			}
+
+			view {
+				flex: 1;
+				margin-left: 16upx;
+				color: #FFFFFF;
+				font-size: 36upx;
+
+				&+view {
+					margin-top: 10upx;
+					font-size: 28upx;
+				}
+			}
+		}
+
+		.bg {
+			position: absolute;
+			left: 0;
+			top: 0;
+			width: 100%;
+			height: 370upx;
+		}
+	}
+
+	.form-box {
+		margin-top: -90upx;
+		border-top-left-radius: 20upx;
+		border-top-right-radius: 20upx;
+		background: #FFFFFF;
+		overflow: hidden;
+		z-index: 2;
+		position: relative;
+		padding: 20upx 32upx;
+
+		.title {
+			font-size: 32upx;
+			line-height: 60upx;
+		}
+	}
+
+	.row {
+		display: flex;
+		align-items: center;
+		position: relative;
+		padding: 0 0upx;
+		height: 90upx;
+		background: #fff;
+
+		&.spec {
+			height: auto;
+
+			.text-area {
+				height: 110upx;
+				padding: 20upx 0;
+				font-size: 30upx;
+			}
+		}
+
+		.tit {
+			flex-shrink: 0;
+			width: 140upx;
+			font-size: 30upx;
+			color: $font-color-dark;
+		}
+
+		.input {
+			flex: 1;
+			font-size: 30upx;
+			color: $font-color-dark;
+		}
+
+		.icon-shouhuodizhi {
+			font-size: 36upx;
+			color: $font-color-light;
+		}
+	}
+	.add-btn {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 690upx;
+		height: 80upx;
+		margin: 60upx auto 20upx;
+		font-size: $font-lg;
+		color: #fff;
+		background-color: $base-color;
+		border-radius: 10upx;
+		box-shadow: 1px 2px 5px rgba(219, 63, 96, 0.4);
+	}
+</style>
---
+++ pages/public/register.vue
@@ -0,0 +1,191 @@
+<template>
+	<view class="content">
+		<view class="title">手机快捷绑定</view>
+		<view class="mobile-input input">
+			<input maxlength="11" type="number" v-model.trim="formData.mobile" class="uni-input" name="mobile" placeholder="请输入手机号"
+			 placeholder-class="placeholder" />
+			<view v-if="show" class="send-code" :class="{ grey: sendLoading }" @click="sendMobile">获取验证码</view>
+			<view v-else class="send-code grey">重新获取（{{ count }}s）</view>
+		</view>
+		<view class="code-input input">
+			<input maxlength="6" type="number" v-model.trim="formData.code" class="uni-input" name="code" placeholder="请输入验证码" placeholder-class="placeholder" />
+		</view>
+		<button class="phone_btn" @click="submit">
+			<view>绑定</view>
+		</button>
+	</view>
+</template>
+
+<script>
+	import validate from '@/utils/validate.js'
+	import {
+		register,
+		sendMsg
+	} from '@/api/phone.js'
+
+	const fields = {
+		mobile: '',
+		code: ''
+	}
+	export default {
+		components: {
+		},
+		data() {
+			return {
+				sendLoading: false,
+				show: true,
+				timer: null,
+				count: 0,
+				formData: JSON.parse(JSON.stringify(fields)),
+				rules: {
+					mobile: {
+						required: true,
+						tel: true
+					},
+					field: {
+						required: true
+					},
+					code: {
+						required: true,
+						minlength: 6,
+						maxlength: 6
+					}
+				},
+				messages: {
+					field: {
+						required: '请先获取短信验证码！'
+					},
+					mobile: {
+						required: '请输入手机号！'
+					},
+					code: {
+						required: '请输入6位短信验证码！',
+						minlength: '请输入6位短信验证码！',
+						maxlength: '请输入6位短信验证码！'
+					}
+				}
+			}
+		},
+		methods: {
+			submit() { // 提交操作
+				const {
+					rules,
+					messages,
+					formData: {
+						mobile,
+						code
+					}
+				} = this
+				const validates = new validate(rules, messages)
+				const valid = validates.checkForm({
+					mobile,
+					code
+				})
+				if (!valid) return
+				const sendData = {
+					phone: mobile
+				}
+				uni.showLoading({
+					title: '加载中',
+					mask: true
+				})
+				bind(sendData).then(async res => {
+					
+				}).catch(e => {
+				})
+			},
+			sendMobile() { // 发送验证码
+				if (this.sendLoading) return
+				const {
+					rules,
+					messages,
+					formData: {
+						mobile
+					}
+				} = this
+				const validates = new validate({
+					mobile: rules.mobile
+				}, messages)
+				const valid = validates.checkForm({
+					mobile
+				})
+				if (!valid) return
+				this.sendLoading = true
+				sendMsg({ type: 0, phone: mobile }).then(res => {
+					uni.setStorageSync('field', res.data)
+					uni.showToast({
+						title: '短信发送成功！',
+						duration: 1500,
+						icon: 'none'
+					})
+					this.sendLoading = false
+					const TIME_COUNT = 60
+					if (!this.timer) {
+						this.count = TIME_COUNT
+						this.show = false
+						this.timer = setInterval(() => {
+							if (this.count > 0 && this.count <= TIME_COUNT) {
+								this.count--
+							} else {
+								this.show = true
+								clearInterval(this.timer)
+								this.timer = null
+							}
+						}, 1000)
+					}
+				}).catch(() => {
+					this.sendLoading = false
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		background: #FFFFFF;
+
+		.title {
+			font-size: 48rpx;
+			line-height: 67rpx;
+			color: #333333;
+			margin: 62rpx 32rpx 50rpx;
+		}
+
+		.input {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			width: 686rpx;
+			position: relative;
+			height: 104rpx;
+			border-bottom: 1rpx solid #EEEEEE;
+			margin: 0 auto;
+
+			.uni-input {
+				flex: 1;
+			}
+
+			.send-code {
+				font-size: 28rpx;
+				color: #333333;
+
+				&.grey {
+					color: #999999;
+				}
+			}
+		}
+
+		.phone_btn {
+			background: #1AAD19;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			margin: 60rpx 32rpx 0;
+			font-size: 36rpx;
+			height: 90rpx;
+			border-radius: 6rpx;
+			color: #FFFFFF;
+		}
+	}
+</style>
